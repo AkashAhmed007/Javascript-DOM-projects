3183a59494a5f040e7b35d1eab094c5c801428ea
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-02-16 13:20:44 +0600

    project4 done

--- javascript/project4.js
+++ javascript/project4.js
@@ -1,7 +1,6 @@
 window.onload = ()=>{
     main();
  }
- 
  function main(){
     const root = document.getElementById("root");
     const changeBtn = document.getElementById("change-btn");
@@ -12,15 +11,23 @@ window.onload = ()=>{
         const bgcolor = generatHEXColor();
         root.style.backgroundColor = bgcolor;
         output.value = bgcolor;
+        generateToastMsg(``);
     })
     copyBtn.addEventListener('click', function(){
         navigator.clipboard.writeText(output.value);
+        generateToastMsg(`Your color : ${output.value} copied`); 
+        output.value = "";
     })
-
  }
  function generatHEXColor(){
     const red = Math.floor(Math.random() * 255);
     const green = Math.floor(Math.random() * 255);
     const blue = Math.floor(Math.random() * 255);
     return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
+}
+function generateToastMsg(msg){
+    const div = document.createElement('div');
+    div.innerText = msg;
+    document.body.appendChild(div);
+    div.classList.add('text-2xl','bg-green-400','w-60','h-24','border-2','fixed','top-8','right-4','p-4');
 }
